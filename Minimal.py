b49275ffbeb755db4dd72852aa6b899e4b29651d
Abul Fahad <117757435+datacode09@users.noreply.github.com>
2024-05-15 13:52:56 -0400

    Update Minimal.py

--- Minimal.py
+++ Minimal.py
@@ -7,17 +7,21 @@ sys.path.insert(0, '/users/prieappatom/')
 from secureconfig import *
 
 def find_teradata_driver():
-    """Find a Teradata driver installed on the system."""
+    """Find the highest version Teradata driver installed on the system."""
     drivers = pyodbc.drivers()
     teradata_drivers = [driver for driver in drivers if 'teradata' in driver.lower()]
     if not teradata_drivers:
         raise ValueError("No Teradata driver found on this system.")
-    return teradata_drivers[0]  # Returns the first matching Teradata driver
+    
+    # Assuming that the driver names include version numbers that can be sorted
+    # Example naming: 'Teradata ODBC Driver 16.20', 'Teradata ODBC Driver 16.10'
+    teradata_drivers.sort(key=lambda name: [int(x) if x.isdigit() else x for x in name.replace('Teradata ODBC Driver ', '').split('.')], reverse=True)
+    return teradata_drivers[0]  # Returns the highest version Teradata driver
 
 def connect_to_teradata():
     uid, pwd = decrypt_credential('SecureConfig_PREU.ini'.format(src))
 
-    driver = find_teradata_driver()  # Find the appropriate Teradata driver
+    driver = find_teradata_driver()  # Find the highest version Teradata driver
     TERADATA_CON = pyodbc.connect(
         f'DRIVER={{{driver}}};DBCNAME=dsn-sysa.rg.rbc.com;UID={uid};PWD={pwd};QUIETMODE=YES;',
         autocommit=True
